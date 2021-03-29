db8c84c3351d02edc3a542c1357be7e989f46041
Taishin <taitasu08035@icloud>
2021-03-29 20:45:54 +0900

    application.js fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
- require ("jquery3")
- require ("popper")
- require ("bootstrap")
+//= require jquery3
+//= require popper
+//= require bootstrap
 
 require("@rails/ujs").start()
 require("turbolinks").start()
